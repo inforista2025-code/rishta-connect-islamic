758e7c97288fa294993052fe952dab01b8e44cf3
inforista2025-code <330408534+inforista2025-code@users.noreply.github.com>
2026-09-23 04:30:45 +0530

    Completely remove AI assistant widget from the website

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,6 @@ import MemberLogin from "./pages/MemberLogin";
 import MemberDashboard from "./pages/MemberDashboard";
 import PremiumDashboard from "./pages/PremiumDashboard";
 import { MemberProtectedRoute } from "./components/MemberProtectedRoute";
-import { IslamicAssistantWidget } from "./components/ai/IslamicAssistantWidget";
 
 const queryClient = new QueryClient();
 
@@ -53,8 +52,6 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-          {/* Global AI Islamic Marriage Assistant Floating Widget */}
-          <IslamicAssistantWidget />
         </BrowserRouter>
       </TooltipProvider>
     </QueryClientProvider>
